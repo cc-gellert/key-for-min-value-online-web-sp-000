6b3aaa0f2474a0dbbce8403665079f590a1772cb
Chloe Gell <gellert.chloe@gmail.com>
2020-01-27 21:31:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = name_hash[0] 
+  lowest_key = Infinity 
   name_hash.each do |key, value|
     if value < lowest_key
       lowest_key = key 
