7c3b1dde600b25c2943a754db8e2c9c1f1ef59ca
Chloe Gell <gellert.chloe@gmail.com>
2020-01-27 21:39:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   end 
   lowest_value = Float::INFINITY 
   lowest_key = ""
-  name_hash.each do |key, value|
+  name_hash.collect do |key, value|
     if value < lowest_value
       lowest_key = key 
     end 
